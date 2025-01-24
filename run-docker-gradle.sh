8f5ba52274df079d84302f1a86ab56f7df964bc5
Florian Kretschmer <florian.kretschmer@viadee.de>
2025-01-24 17:52:48 +0100

    add reproducible build for gradle

---
+++ run-docker-gradle.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+cd "$SCRIPT_DIR"
+
+docker build -f Dockerfile.gradle -t deleteme --progress=plain .
